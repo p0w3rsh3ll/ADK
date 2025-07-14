c11773eb8fbeb1303d8f755ad491660899d128b2
Emin <emin.atac@gmail.com>
2025-07-14 18:22:00 +0200

    Testing issue #4 on adkwinpesetup 10.1.25398.1

--- 10.1.25398.1/Get-ADKWinPEAddons.ps1
+++ 10.1.25398.1/Get-ADKWinPEAddons.ps1
@@ -1,4 +1,4 @@
-#Requires -Version 4
+﻿#Requires -Version 4
 #Requires -RunAsAdministrator
 
 Function Get-ADKWinPEAddonsFiles {
@@ -8,9 +8,9 @@ Param(
     [system.string]$TargetFolder
 )
 Begin {
-    
+
     $HT = @{ ErrorAction = 'Stop'}
-    
+
     # Validate target folder
     try {
         $null = Get-Item $TargetFolder @HT
@@ -23,12 +23,12 @@ Begin {
 Process {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $adkGenericURL = (Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?linkid=2223417' -MaximumRedirection 0 -ErrorAction SilentlyContinue)
-    
+
     # 302 = redirect as moved temporarily
     # 301 = Moved Permanently
     if ($adkGenericURL.StatusCode -eq 302) {
 
-        # Currently set to https://download.microsoft.com/download/c/6/8/c68972f8-9148-4240-818e-7288e1e54256/adkwinpeaddons
+        # Currently set to https://download.microsoft.com/download/4/7/6/47653b91-4dc7-46f3-ad00-e665f6ac8105/ADKWINPEADDONS/
         # Resolving download root for: https://go.microsoft.com/fwlink/?linkid=2223417
         $MainURL = $adkGenericURL.Headers.Location
 
@@ -37,7 +37,7 @@ Process {
         if(-not ($MainURL.EndsWith('/'))) {
             $MainURL = "$($MainURL)/"
         }
-    
+
         $InstallerURLs = DATA {
             ConvertFrom-StringData @'
 0=0b63b7c537782729483bff2d64a620fa.cab
@@ -55,11 +55,12 @@ Process {
 12=e7a23bd4ab45aa717a3e0b2fa877dce9.cab
 13=f493bb83bbe844f5cd3610b0847bc113.cab
 14=f53c2cdf2f5aeeaef4ca19d4b3344930.cab
-15=Windows PE Boot Files (DesktopEditions)-x86_en-us.msi
-16=Windows PE Boot Files (OnecoreUAP)-x86_en-us.msi
-17=Windows PE Optional Packages (DesktopEditions)-x86_en-us.msi
-18=Windows PE Scripts-x86_en-us.msi
-19=Windows PE wims (DesktopEditions)-x86_en-us.msi
+15=Kits Configuration Installer-x86_en-us.msi
+16=Windows PE Boot Files (DesktopEditions)-x86_en-us.msi
+17=Windows PE Boot Files (OnecoreUAP)-x86_en-us.msi
+18=Windows PE Optional Packages (DesktopEditions)-x86_en-us.msi
+19=Windows PE Scripts-x86_en-us.msi
+20=Windows PE wims (DesktopEditions)-x86_en-us.msi
 '@
         }
 
@@ -83,7 +84,7 @@ Process {
             Write-Warning -Message "Failed to download adkwinpesetup.exe because $($_.Exception.Message)"
             break
         }
-    
+
         # Create a job that will downlad our first file
         $ffsource = "$($MainURL)Installers/$($InstallerURLs['0'])"
         $ffdest   = (Join-Path -Path $TargetFolder -ChildPath ("Installers/$($InstallerURLs['0'])"))
@@ -101,7 +102,7 @@ Process {
             Write-Warning -Message "Failed to create first BITS job because $($_.Exception.Message)"
             break
         }
-            
+
         # Downlod installers
         For ($i = 1 ; $i -lt $InstallerURLs.Count ; $i++) {
             $URL = $Destination = $null
@@ -111,15 +112,15 @@ Process {
             # Add-BitsFile http://technet.microsoft.com/en-us/library/dd819411.aspx
             $newjob = Add-BitsFile -BitsJob $job -Source  $URL -Destination $Destination
             Write-Progress -Activity "Adding file $($newjob.FilesTotal)" -Status "Percent completed: " -PercentComplete (($newjob.FilesTotal)*100/($InstallerURLs.Count))
-        } 
+        }
 
         # Begin the download and show us the job
         $null = Resume-BitsTransfer  -BitsJob $job -Asynchronous
-    
+
         # http://msdn.microsoft.com/en-us/library/windows/desktop/ee663885%28v=vs.85%29.aspx
         while ($job.JobState -in @('Connecting','Transferring','Queued')) {
             Write-Progress -activity 'Downloading ADK files' -Status 'Percent completed: ' -PercentComplete ($job.BytesTransferred*100/$job.BytesTotal)
-        } 
+        }
         Switch($job.JobState) {
              'Transferred' {
                 Complete-BitsTransfer -BitsJob $job
@@ -129,10 +130,10 @@ Process {
              'Error' {
                 # List the errors.
                 $job | Format-List
-            } 
+            }
             default {
                 # Perform corrective action.
-            } 
+            }
         }
     } else {
         Write-Warning -Message "Guessing the ADK location returned the status code $($adkGenericURL.StatusCode)"
